f521c5bfb0aa5a4a5c62aa4830b01492956f03d8
Pham Tu <tubackkhoa@gmail.com>
2023-08-02 16:46:31 +0700

    add testcase

--- packages/subscription-sdk/src/index.spec.ts
+++ packages/subscription-sdk/src/index.spec.ts
@@ -1,6 +1,7 @@
 import * as subscriptionArtifacts from '@oraichain/subscription-contracts-build';
 import { CosmwasmSubscriptionClient } from '@oraichain/subscription-contracts-sdk';
 import { SimulateCosmWasmClient } from '@oraichain/cw-simulate';
+import { coin, coins } from '@cosmjs/amino';
 
 const client = new SimulateCosmWasmClient({
   bech32Prefix: 'orai',
@@ -9,16 +10,34 @@ const client = new SimulateCosmWasmClient({
 });
 const senderAddress = 'orai12zyu8w93h0q2lcnt50g3fn0w3yqnhy4fvawaqz';
 
-describe('simple case', () => {
+describe('simple_case', () => {
   let subscriptionContract: CosmwasmSubscriptionClient;
 
   beforeEach(async () => {
     const { contractAddress } = await subscriptionArtifacts.deployContract(client, senderAddress, {}, 'cosmwasm-subscription');
+    client.app.bank.setBalance(senderAddress, coins('100000000', 'orai'));
     subscriptionContract = new CosmwasmSubscriptionClient(client, senderAddress, contractAddress);
   });
 
   it('works', async () => {
     let subscriptionOptions = await subscriptionContract.subscriptionOptions();
     console.log(subscriptionOptions);
+  });
+
+  it('add_default_subscription_option', async () => {
+    // 1 day, 10_000_000 orai
+    let result = await subscriptionContract.admin({
+      add_subscription_option: {
+        payment_option: {
+          subscription_duration: {
+            duration_unit: 'day',
+            amount_units: 1
+          },
+          price: coin('10000000', 'orai')
+        }
+      }
+    });
+    expect(result.events.length).toBeGreaterThan(0);
+    console.log(result);
   });
 });
